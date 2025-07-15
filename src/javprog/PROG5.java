66b47cae506ab55f48678bc8bce087ea5fad5d7d
Raghu <gunjar1999@gmail.com>
2025-07-15 20:11:35 +0530

    add this file

--- src/javprog/PROG5.java
+++ src/javprog/PROG5.java
@@ -1,6 +1,6 @@
 package javprog;
 
-public class prog4 {
+public class PROG5 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println(2+5);
