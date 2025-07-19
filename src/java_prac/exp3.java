ba275456a029b24064620cd51eeaa63452b6dea5
Raghu <gunjar1999@gmail.com>
2025-07-19 19:00:46 +0530

    add this file

---
+++ src/java_prac/exp3.java
@@ -0,0 +1,15 @@
+package java_prac;
+
+public class exp3 {
+    public static void main(String[] args) {
+        int a = 35;
+        int b = 5;
+        System.out.println(a-b);
+        System.out.println(a%b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a+b);
+
+
+    }
+}
