43ab61256ee52c32c85a8242b424696bc525e805
Raghu <gunjar1999@gmail.com>
2025-07-19 20:21:36 +0530

    add this file

---
+++ src/java_prac/exp4.java
@@ -0,0 +1,18 @@
+package java_prac;
+
+public class exp4 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 2;
+        System.out.printf("%d-%d=%d",a,b,a-b);
+        System.out.println();
+        System.out.printf("%d+%d=%d",a,b,a+b);
+        System.out.println();
+        System.out.printf("%d/%d=%d",a,b,a/b);
+        System.out.println();
+        System.out.printf("%d*%d=%d",a,b,a*b);
+        System.out.println();
+
+
+    }
+}
