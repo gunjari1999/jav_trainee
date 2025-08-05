14ca465838852f0c9e9b4b95fb97b2511e9fbd29
Raghu <gunjar1999@gmail.com>
2025-08-05 13:54:56 +0530

    add this file

---
+++ src/ex_ples1/even_do.java
@@ -0,0 +1,19 @@
+package ex_ples1;
+
+public class even_do {
+    public static void main(String[] args) {
+//        int sum = 0;
+//        for(int a=1;a<=10;a++){
+//            sum +=2 ;
+//                System.out.println(sum);
+
+  //      }
+            int sum = 1;
+            for(int a=1;a<=5;a++) {
+               sum *= a;
+                System.out.println(sum);
+            }
+
+
+    }
+}
